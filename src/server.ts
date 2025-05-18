e92b8b464cf35cbc359edc711b3e1de772918dff
BernardoVieira1 <bernardojvieira50@gmail.com>
2025-05-18 15:05:31 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/user.routes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Aqui depois vamos adicionar as rotas
+app.get('/', (req, res) => {
+    res.send('API funcionando 🚀');
+});
+
+
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ src/services/user.service.ts
@@ -0,0 +1,34 @@
+import prisma from '../config/prisma';
+import bcrypt from 'bcrypt';
+
+interface CreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const createUser = async (data: CreateUserDTO) => {
+    const emailExistente = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    }) 
+
+  if(emailExistente){
+    throw new Error('Email já cadastrado');
+  } 
+
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  return await prisma.user.create({
+    data:{
+        email: data.email,
+        name: data.name,
+        password: hashedPassword,
+    }
+  });
+};
+
+export default {
+  createUser,
+};
